233127ee8c7065ac953200e7104f056ff0db15b6
Francis Ngo <rr239696@CA0727AM056322.na.ad.rrd.com>
2016-08-11 22:06:26 -0700

    initial commit for exercise 1 and 2 solutions

---
+++ Basic Algorithm Scripting/ReverseAString.js
@@ -0,0 +1,18 @@
+/*
+Reverse A String
+
+Reverse the provided string.
+
+You may need to turn the string into an array before you can reverse it.
+
+Your result must be a string.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+*/
+
+//Solution
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+reverseString("hello");
---
+++ README.md
@@ -0,0 +1,2 @@
+# freecodecamp_solutions
+Algorithm Solutions
